63174a6793b00d7c73e97a00851cd7f049763313
cerdemrc <cerdemrc@gmail.com>
2021-08-24 12:05:23 +0300

    add draggable in list

--- src/components/List.vue
+++ src/components/List.vue
@@ -9,238 +9,148 @@
       >
         <div class="card-header">
           Todo
-          <span class="badge rounded-pill bg-warning text-dark">4</span>
+          <span class="badge rounded-pill bg-warning text-dark">{{
+            getTodoCount ? getTodoCount : "0"
+          }}</span>
         </div>
-        <div class="card-middle" v-for="i in 4" :key="i">
-          <div class="card-content">
-            <h4>Learn to code</h4>
-            <button class="btn">
-              <i class="bi bi-trash card-content-icon"></i>
-            </button>
+        <draggable>
+          <div class="card-middle" v-for="item in getTask(1)" :key="item.id">
+            <div
+              class="card-content"
+              :style="{ 'background-color': item.color }"
+            >
+              <h4>{{ item.task }}</h4>
+              <button class="btn" @click="deleteTask(item.id)">
+                <i class="bi bi-trash card-content-icon"></i>
+              </button>
+            </div>
           </div>
-        </div>
+        </draggable>
       </div>
     </div>
     <div class="col-xs-12 col-md-4">
       <div
         class="card text-center"
-        @drop="onDrop($event, 1)"
+        @drop="onDrop($event, 2)"
         @dragenter.prevent
         @dragover.prevent
       >
         <div class="card-header">
           In Progress
-          <span class="badge rounded-pill bg-warning text-dark">2</span>
+          <span class="badge rounded-pill bg-warning text-dark">{{
+            getInProgressCount ? getInProgressCount : "0"
+          }}</span>
         </div>
-        <div class="card-middle" v-for="i in 2" :key="i">
-          <div class="card-content">
-            <h4>Learn to code</h4>
-            <button class="btn">
-              <i class="bi bi-trash card-content-icon"></i>
-            </button>
+        <draggable>
+          <div
+            class="card-middle"
+            v-for="item in getTask(2)"
+            :key="item.id"
+            draggable
+            @dragstart="startDrag($event, item)"
+          >
+            <div
+              class="card-content"
+              :style="{ 'background-color': item.color }"
+            >
+              <h4>{{ item.task }}</h4>
+              <button class="btn" @click="deleteTask(item.id)">
+                <i class="bi bi-trash card-content-icon"></i>
+              </button>
+            </div>
           </div>
-        </div>
+        </draggable>
       </div>
     </div>
     <div class="col-xs-12 col-md-4">
       <div
         class="card text-center"
-        @drop="onDrop($event, 1)"
+        @drop="onDrop($event, 3)"
         @dragenter.prevent
         @dragover.prevent
       >
         <div class="card-header">
           Done
-          <span class="badge rounded-pill bg-warning text-dark">1</span>
+          <span class="badge rounded-pill bg-warning text-dark">{{
+            getDoneCount ? getDoneCount : "0"
+          }}</span>
         </div>
-        <div class="card-middle">
-          <div class="card-content">
-            <h4>Learn to code</h4>
-            <button class="btn">
-              <i class="bi bi-trash card-content-icon"></i>
-            </button>
+        <draggable>
+          <div
+            class="card-middle"
+            v-for="item in getTask(3)"
+            :key="item.id"
+            draggable
+            @dragstart="startDrag($event, item)"
+          >
+            <div
+              class="card-content"
+              :style="{ 'background-color': item.color }"
+            >
+              <h4>{{ item.task }}</h4>
+              <button class="btn" @click="deleteTask(item.id)">
+                <i class="bi bi-trash card-content-icon"></i>
+              </button>
+            </div>
           </div>
-        </div>
+        </draggable>
       </div>
     </div>
   </div>
-  <!--<v-row class="list">
-    <v-col cols="12" lg="4">
-      <v-card
-        max-width="400"
-        class="mx-auto list-card"
-        @drop="onDrop($event, 1)"
-        @dragenter.prevent
-        @dragover.prevent
-      >
-        <v-app-bar dark color="black">
-          <v-badge
-            color="orange"
-            inline
-            :content="getTodoCount ? getTodoCount : '0'"
-          >
-            To Do
-          </v-badge>
-        </v-app-bar>
-
-        <v-container>
-          <v-row dense>
-            <v-col
-              v-for="item in getTask(1)"
-              :key="item.id"
-              cols="12"
-              draggable
-              @dragstart="startDrag($event, item)"
-            >
-              <v-card :color="item.color" dark class="list-card-content">
-                <v-card-actions>
-                  <div>
-                    <v-card-title
-                      class="text-h5"
-                      v-text="item.task"
-                    ></v-card-title>
-                  </div>
-                  <v-spacer></v-spacer>
-
-                  <v-btn icon @click="deleteTask(item.id)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-    <v-col cols="12" lg="4">
-      <v-card
-        max-width="400"
-        class="mx-auto list-card"
-        @drop="onDrop($event, 2)"
-        @dragenter.prevent
-        @dragover.prevent
-      >
-        <v-app-bar dark color="black">
-          <v-badge
-            color="orange"
-            inline
-            :content="getInProgressCount ? getInProgressCount : '0'"
-          >
-            In Progress
-          </v-badge>
-        </v-app-bar>
-
-        <v-container>
-          <v-row dense>
-            <v-col
-              v-for="item in getTask(2)"
-              :key="item.id"
-              cols="12"
-              draggable
-              @dragstart="startDrag($event, item)"
-            >
-              <v-card :color="item.color" dark class="list-card-content">
-                <v-card-actions>
-                  <div>
-                    <v-card-title
-                      class="text-h5"
-                      v-text="item.task"
-                    ></v-card-title>
-                  </div>
-                  <v-spacer></v-spacer>
-
-                  <v-btn icon @click="deleteTask(item.id)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-    <v-col cols="12" lg="4">
-      <v-card
-        max-width="400"
-        class="mx-auto list-card"
-        @drop="onDrop($event, 3)"
-        @dragenter.prevent
-        @dragover.prevent
-      >
-        <v-app-bar dark color="black">
-          <v-badge
-            color="orange"
-            inline
-            :content="getDoneCount ? getDoneCount : '0'"
-          >
-            Done
-          </v-badge>
-        </v-app-bar>
-
-        <v-container>
-          <v-row dense>
-            <v-col
-              v-for="item in getTask(3)"
-              :key="item.id"
-              cols="12"
-              draggable
-              @dragstart="startDrag($event, item)"
-            >
-              <v-card :color="item.color" dark class="list-card-content">
-                <v-card-actions>
-                  <div>
-                    <v-card-title
-                      class="text-h5"
-                      v-text="item.task"
-                    ></v-card-title>
-                  </div>
-                  <v-spacer></v-spacer>
-
-                  <v-btn icon @click="deleteTask(item.id)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-col>
-  </v-row>-->
 </template>
 
-//<script>
-//import { mapGetters } from "vuex";
-//export default {
-//  name: "List",
-//  computed: {
-//    ...mapGetters([
-//      "getAllTasks",
-//      "getTodoCount",
-//      "getInProgressCount",
-//      "getDoneCount",
-//    ]),
-//  },
-//  methods: {
-//    deleteTask(id) {
-//      this.$store.commit("deleteTask", id);
-//    },
-//    getTask(list) {
-//      return this.getAllTasks.filter((task) => task.list == list);
-//    },
-//    startDrag(evt, item) {
-//      evt.dataTransfer.dropEffect = "move";
-//      evt.dataTransfer.effectAllowed = "move";
-//      evt.dataTransfer.setData("itemID", item.id);
-//    },
-//    onDrop(evt, list) {
-//      const itemID = evt.dataTransfer.getData("itemID");
-//      const item = this.getAllTasks.find((item) => item.id == itemID);
-//      item.list = list;
-//    },
-//  },
-//};
-//
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import { mapGetters } from "vuex";
+import { VueDraggableNext } from "vue-draggable-next";
+
+export default {
+  components: {
+    draggable: VueDraggableNext,
+  },
+  setup() {
+    const store = useStore();
+    function deleteTask(id) {
+      store.commit("deleteTask", id);
+    }
+    function getTask(list) {
+      return getAllTasks.value.filter((task) => task.list == list);
+    }
+    function startDrag(evt, item) {
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("itemID", item.id);
+    }
+    function onDrop(evt, list) {
+      const itemID = evt.dataTransfer.getData("itemID");
+      const item = getAllTasks.value.find((item) => item.id == itemID);
+      item.list = list;
+    }
+    const getAllTasks = computed(function () {
+      return store.getters.getAllTasks;
+    });
+    const getTodoCount = computed(function () {
+      return store.getters.getTodoCount;
+    });
+    const getInProgressCount = computed(function () {
+      return store.getters.getInProgressCount;
+    });
+    const getDoneCount = computed(function () {
+      return store.getters.getDoneCount;
+    });
+    return {
+      deleteTask,
+      getTask,
+      startDrag,
+      onDrop,
+      getAllTasks,
+      getTodoCount,
+      getInProgressCount,
+      getDoneCount,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -258,9 +168,8 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background: sandybrown;
     color: #fff;
-    padding: 5px;
+    padding: 5px 15px;
     border-radius: 10px;
     transform: translate(0, -1px);
     transition: 0.2s;
@@ -270,7 +179,8 @@
       transform: translate(0, 1px);
     }
     &-icon {
-      font-size: 1.5rem;
+      font-size: 1.2rem;
+      color: #fff;
     }
   }
 }
