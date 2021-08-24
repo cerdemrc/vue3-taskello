4ab8e39856a57aa4c6b8574c270fa91717af0d34
cerdemrc <cerdemrc@gmail.com>
2021-08-24 12:04:42 +0300

    bind operations

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -32,25 +32,30 @@
           <form>
             <div class="row">
               <div class="col-12">
-                <input class="form-control" type="text" placeholder="Task" />
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Task"
+                  v-model="newTask.task"
+                />
                 <!--<small v-if="!$v.newTask.task.required" class="red--text"
                   >Task is required</small
                 >-->
               </div>
               <div class="col-6 mt-3">
-                <select class="form-select">
+                <select class="form-select" v-model="newTask.list">
                   <option selected disabled>List</option>
-                  <option>Todo</option>
-                  <option>In Progress</option>
-                  <option>Done</option>
+                  <option v-for="list in lists" :key="list.id">
+                    {{ list.list }}
+                  </option>
                 </select>
               </div>
               <div class="col-6 mt-3">
-                <select class="form-select">
+                <select class="form-select" v-model="newTask.color">
                   <option selected disabled>Color</option>
-                  <option>Todo</option>
-                  <option>In Progress</option>
-                  <option>Done</option>
+                  <option v-for="color in colors" :key="color.id">
+                    {{ color.color }}
+                  </option>
                 </select>
               </div>
             </div>
@@ -61,10 +66,13 @@
             type="button"
             class="btn btn-secondary"
             data-bs-dismiss="modal"
+            @click="closeDialog"
           >
             Close
           </button>
-          <button type="button" class="btn btn-success">Add</button>
+          <button type="button" class="btn btn-success" @click="saveTask">
+            Add
+          </button>
         </div>
       </div>
     </div>
@@ -72,67 +80,109 @@
 </template>
 
 <script>
-import { validationMixin } from "vuelidate";
-import { required } from "vuelidate/lib/validators";
+//import { validationMixin } from "vuelidate";
+//import { required } from "vuelidate/lib/validators";
+import { ref, reactive } from "vue";
+import { useStore } from "vuex";
 export default {
-  mixins: [validationMixin],
-  validations: {
-    newTask: {
-      task: { required },
-      list: { required },
-    },
-  },
-  data() {
-    return {
-      dialog: false,
-      newTask: {
-        task: "",
-        list: null,
-        color: null,
-      },
-      lists: [
-        { list: "Todo", id: 1 },
-        { list: "In Progress", id: 2 },
-        { list: "Done", id: 3 },
-      ],
-      colors: [
-        { color: "Red", id: "red" },
-        { color: "Blue", id: "blue" },
-        { color: "Green", id: "green" },
-        { color: "Pink", id: "pink" },
-        { color: "Purple", id: "purple" },
-        { color: "Orange", id: "orange" },
-        { color: "Gray", id: "gray" },
-        { color: "Black", id: "black" },
-      ],
-    };
-  },
-  methods: {
-    saveTask() {
-      this.$v.$touch();
-      if (!this.$v.$invalid) {
-        const id = Math.floor(Math.random() * 100000);
-        this.$store.commit("saveTask", { ...this.newTask, id });
-        this.dialog = false;
+  setup() {
+    const store = useStore();
+    const dialog = ref(false);
+    const newTask = reactive({ task: "", list: null, color: null });
+    const lists = reactive([
+      { list: "Todo", id: 1 },
+      { list: "In Progress", id: 2 },
+      { list: "Done", id: 3 },
+    ]);
+    const colors = reactive([
+      { color: "Red", id: "red" },
+      { color: "Blue", id: "blue" },
+      { color: "Green", id: "green" },
+      { color: "Pink", id: "pink" },
+      { color: "Purple", id: "purple" },
+      { color: "Orange", id: "orange" },
+      { color: "Gray", id: "gray" },
+      { color: "Black", id: "black" },
+    ]);
 
-        //cleaning
-        this.newTask.task = "";
-        this.newTask.list = "";
-        this.newTask.color = "";
-      }
-    },
-    closeDialog() {
+    function saveTask() {
+      const id = Math.floor(Math.random() * 100000);
+      store.commit("saveTask", { ...this.newTask, id });
+      this.dialog = false;
+
+      //cleaning
+      //  this.newTask.task = "";
+      //  this.newTask.list = "";
+      //  this.newTask.color = "";
+    }
+
+    function closeDialog() {
       this.dialog = false;
 
       //cleaning
       this.newTask.task = "";
       this.newTask.list = "";
       this.newTask.color = "";
-    },
-    submit() {
-      this.$v.$touch();
-    },
+    }
+
+    return { dialog, newTask, lists, colors, saveTask, closeDialog };
   },
+  //  mixins: [validationMixin],
+  //  validations: {
+  //    newTask: {
+  //      task: { required },
+  //      list: { required },
+  //    },
+  //  },
+  //  data() {
+  //    return {
+  //      dialog: false,
+  //      newTask: {
+  //        task: "",
+  //        list: null,
+  //        color: null,
+  //      },
+  //      lists: [
+  //        { list: "Todo", id: 1 },
+  //        { list: "In Progress", id: 2 },
+  //        { list: "Done", id: 3 },
+  //      ],
+  //      colors: [
+  //        { color: "Red", id: "red" },
+  //        { color: "Blue", id: "blue" },
+  //        { color: "Green", id: "green" },
+  //        { color: "Pink", id: "pink" },
+  //        { color: "Purple", id: "purple" },
+  //        { color: "Orange", id: "orange" },
+  //        { color: "Gray", id: "gray" },
+  //        { color: "Black", id: "black" },
+  //      ],
+  //    };
+  //  },
+  //  methods: {
+  //    saveTask() {
+  //      this.$v.$touch();
+  //      if (!this.$v.$invalid) {
+  //        const id = Math.floor(Math.random() * 100000);
+  //        this.$store.commit("saveTask", { ...this.newTask, id });
+  //        this.dialog = false;
+  //        //cleaning
+  //        this.newTask.task = "";
+  //        this.newTask.list = "";
+  //        this.newTask.color = "";
+  //      }
+  //    },
+  //    closeDialog() {
+  //      this.dialog = false;
+  //      //cleaning
+  //      this.newTask.task = "";
+  //      this.newTask.list = "";
+  //      this.newTask.color = "";
+  //    },
+  //    submit() {
+  //      this.$v.$touch();
+  //    },
+  //  },
 };
 </script>
 
